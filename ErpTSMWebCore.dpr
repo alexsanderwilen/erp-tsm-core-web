bcf863dc44d2883bdbdc329807ffeb3dcc23bff0
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2026-04-01 09:29:46 -0300

    Criação inicial do Projeto

---
+++ ErpTSMWebCore.dpr
@@ -0,0 +1,19 @@
+program ErpTSMWebCore;
+
+
+
+{$R *.dres}
+
+uses
+  Vcl.Forms,
+  WEBLib.Forms,
+  uPrincipal in 'uPrincipal.pas' {Form1: TWebForm} {*.html};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
